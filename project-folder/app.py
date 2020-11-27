8e48f0d6963d328b27a7eb9804a4ab69be7217ef
berryny <judesire@yahoo.com>
2020-11-27 16:58:27 -0500

    db coding

--- project-folder/app.py
+++ project-folder/app.py
@@ -6,6 +6,9 @@ from flask import Flask, request, redirect, url_for, render_template, session, j
 from flask_assets import Environment, Bundle
 # Initialize the app by creating an Environment instance, and registering your assets with it in the form of so called bundles.
 
+# Quickstart Flask-SQLAlchemy https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+from flask_sqlalchemy import SQLAlchemy
+
 from bs4 import BeautifulSoup
 import requests, json, urllib3
 from urllib.parse import urlparse, urljoin, urlunparse
@@ -73,35 +76,35 @@ class WebPagesPreview(object):
                 if tagTitle is not None:
                     dataMeta['title'] = tagTitle.text
 
-                # Site Meta information
-                tagMetaDescription = soup.find('meta', attrs={'property' : 'og:description'})
-                if tagMetaDescription is not None:
-                    dataMeta['metadescription'] = tagMetaDescription.text
-
-                tagMetaImage = soup.find('meta', attrs={'property' : 'og:image'})
-                if tagMetaImage is not None and 'content' in tagMetaImage.attrs:
-                    dataMeta['metaimage'] = urljoin(urlBase, tagMetaImage['content'])
-
-                tagRelImage = soup.find('rel', attrs={'property' : 'shortcut icon'})
-                if tagRelImage is not None and 'href' in tagRelImage.attrs:
-                    dataMeta['relimage'] = urljoin(urlBase, tagRelImage['href'])
-
+                # Site Meta or Description information
                 tagBody = soup.find('body')
                 tagDescription = tagBody.find('p')
+                tagMetaDescription = soup.find('meta', attrs={'property' : 'og:description'})
                 if tagDescription is not None:
                     dataMeta['description'] = tagDescription.text
+                elif tagMetaDescription is not None:
+                    dataMeta['description'] = tagMetaDescription.text
+                else:
+                    dataMeta['description'] = None
 
+                tagMetaImage = soup.find('meta', attrs={'property' : 'og:image'})
+                tagRelImage = soup.find('rel', attrs={'property' : 'shortcut icon'})
                 tagImage = tagBody.find('img')
-                # im = []
-                # for ig in tagBody.find_all('img'):
-                #     # im = Image.open(urljoin(urlBase, ig['src']))
-                #     filename = urljoin(urlBase, ig['src'])
-                #     req = requests.get(filename)
-                #     if req.status_code == 200:
-                #         im.append(ig)
-
                 if tagImage is not None and 'src' in tagImage.attrs:
+                    # im = []
+                    # for ig in tagBody.find_all('img'):
+                    #     # im = Image.open(urljoin(urlBase, ig['src']))
+                    #     filename = urljoin(urlBase, ig['src'])
+                    #     req = requests.get(filename)
+                    #     if req.status_code == 200:
+                    #         im.append(ig)
                     dataMeta['image'] = urljoin(urlBase, tagImage['src'])
+                elif tagMetaImage is not None and 'content' in tagMetaImage.attrs:
+                    dataMeta['image'] = urljoin(urlBase, tagMetaImage['content'])
+                elif tagRelImage is not None and 'href' in tagRelImage.attrs:
+                    dataMeta['image'] = urljoin(urlBase, tagRelImage['href'])
+                else:
+                    dataMeta['image'] = None
 
                 siteMeta.append(dataMeta)
         # print('dataMeta',siteMeta)
@@ -109,6 +112,31 @@ class WebPagesPreview(object):
 
 app = Flask(__name__)
 
+app.config["ENV"] = 'development'
+app.config["SECRET_KEY"]=b'_5#y2L"F4Q8z\n\xec]/'
+
+# change the following .db file name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitepreview.db'
+# this line is to prevent SQLAlchemy from throwing a warning
+# if you don't get one with out it, feel free to remove
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#
+# DB SETUP
+#
+
+# this set's up our db connection to our flask application
+db = SQLAlchemy(app)
+# this is our model (aka table)
+class PreviewDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.LargeBinary, nullable=False)
+
+    def __repr__(self):
+        return '<PreviewDB %r>' % self.title
 bundles = {
 
     'all_js': Bundle(
@@ -136,6 +164,25 @@ def nav(categoryItem):
     catLinks = l.dataCategory(categoryItem.lower())
     sitepreview = l.scrapSite(catLinks)
     print('sitepreview',sitepreview)
+
+    # previewList = []
+    # for previews in sitepreview:
+    #     previewDict = {}
+    #     for p in previews:
+    #         print('previews',p,previews[p])
+    #         previewDict[p] = previews[p]
+    #         previewDict['category'] = categoryItem
+    #         previewList.append(previewDict)
+    #
+
+    for pLst in sitepreview:
+        print('pLst',pLst)
+        s = PreviewDB(category = categoryItem.lower(), title = pLst['title'], description = pLst['description'], image = pLst['image'])
+        try:
+            db.session.add(s)
+            db.session.commit()
+        except:
+            print("not gonna work")
     return render_template("category.html", title=categoryItem, sitepreview=sitepreview)
 
 @app.errorhandler(404)
